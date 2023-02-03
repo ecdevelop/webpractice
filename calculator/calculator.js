e689004137e2412eb4693e7d04f5c3a755c368a2
Spencer.Robles <crazymeet1998@gmail.com>
2023-02-03 20:51:21 +0800

    calc

---
+++ calculator/calculator.js
@@ -0,0 +1,43 @@
+let p = document.querySelector(".screen");
+let pvalue = document.querySelector(".screen").innerText;
+
+function getp(){
+    console.log(p);
+}
+
+function getNumberOne(){
+    p.innerHTML =   pvalue = '' +pvalue+1;
+}
+
+function getNumberOne(){
+    p.innerHTML =   pvalue = '' +pvalue+1;
+}
+
+
+
+// const buttonGroup = document.getElementById("button-group");
+// const result = document.getElementById("result");
+
+// const buttonGroupPressed = e => { 
+  
+//   const isButton = e.target.nodeName === 'BUTTON';
+  
+//   if(!isButton) {
+//     return
+//   }
+  
+//    result.innerHTML = `ID of <em>${e.target.innerHTML}</em> is <strong>${e.target.id}</strong>`;
+  
+// }
+// buttonGroup.addEventListener("click", buttonGroupPressed);
+
+
+/* <div id="button-group">
+  <button id="button-1">Button 1</button>
+  <button id="button-2">Button 2</button>
+  <button id="button-3">Button 3</button>
+  <button id="button-4">Button 4</button>
+  <button id="button-5">Button 5</button>
+</div>
+
+<div id="result"></div> */
