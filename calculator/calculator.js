c637e00ef4973cf73281dbbcecd2979003410a0f
Spencer Lanz G. Robles <spencer-lanz.robles@atos.net>
2023-02-04 16:38:09 +0800

    calc

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -1,4 +1,4 @@
-let p = document.querySelector(".screen");
+// let p = document.querySelector(".screen");
 let pvalue = document.querySelector(".screen").innerText;
 
 function getp(){
@@ -12,6 +12,40 @@ function getNumberOne(){
 function getNumberOne(){
     p.innerHTML =   pvalue = '' +pvalue+1;
 }
+
+function clear(){
+    p.innerText = '0';
+}
+
+const buttonGroup = document.getElementById("button-group");
+let p = document.querySelector(".screen");
+
+const buttonGroupPressed = e => { 
+
+  const isButton = e.target.nodeName === 'BUTTON';
+  if(!isButton) {
+    return
+  }
+  
+     p.innerHTML = pvalue =''+pvalue+ e.target.innerHTML;
+    // if(p.innerHTML === '0'){
+    //     alert("zero");
+    //     p.innerHTML =  e.target.innerHTML ;
+    // }  
+
+}
+buttonGroup.addEventListener("click", buttonGroupPressed);
+
+
+
+/* <div id="button-group">
+  <button id="button-1">Button 1</button>
+  <button id="button-2">Button 2</button>
+  <button id="button-3">Button 3</button>
+  <button id="button-4">Button 4</button>
+  <button id="button-5">Button 5</button>
+</div>
+<div id="result"></div> */
 
 
 
@@ -31,13 +65,3 @@ function getNumberOne(){
 // }
 // buttonGroup.addEventListener("click", buttonGroupPressed);
 
-
-/* <div id="button-group">
-  <button id="button-1">Button 1</button>
-  <button id="button-2">Button 2</button>
-  <button id="button-3">Button 3</button>
-  <button id="button-4">Button 4</button>
-  <button id="button-5">Button 5</button>
-</div>
-
-<div id="result"></div> */
